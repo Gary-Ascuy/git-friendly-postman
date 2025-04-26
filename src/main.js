f50c6e9af462d45ac4e79623ae4f588797c544fc
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-25 23:09:49 -0500

    Adding base command implementation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
 
-async function main() {
+const { Command } = require('commander')
+const program = new Command()
+
+program
+    .name('git-friendly-postman')
+    .description('Git Friendly Postman Tool')
+    .version('0.1.0')
+    .option('--clean')
+    .option('-e,--export <path>')
+    .option('-f,--file <path>')
+    .argument('<string>')
+
+program.parse()
+const options = program.opts()
+
+async function main(path, options) {
+    console.log(path)
+    console.log(options)
 }
 
-main()
+main(options.file ?? program.args[0], options)
