eb3dc1976284b30eedc11f75108eddde16571392
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-26 00:31:44 -0500

    Adding version

--- src/commands/constants.js
+++ src/commands/constants.js
@@ -1,4 +1,6 @@
-export const VERSION = '0.1.0'
+import { version } from './package.json';
+
+export const VERSION = "0.4.0"
 export const AUTHOR = 'Gary Ascuy'
 
 export const DESCRIPTION = 'Git Friendly Postman Tool'
